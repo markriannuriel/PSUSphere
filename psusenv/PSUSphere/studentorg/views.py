1495fb66a61c84db4dddb0b23ec22f47112890c0
Fernandez <202180420@psu.palawan.edu.ph>
2026-02-23 22:11:37 +0800

    feat: add Organization update view and edit link in list

--- psusenv/PSUSphere/studentorg/views.py
+++ psusenv/PSUSphere/studentorg/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
@@ -21,6 +21,13 @@ class OrganizationList(ListView):
 
 
 class OrganizationCreateView(CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_form.html'
+    success_url = reverse_lazy('organization-list')
+
+
+class OrganizationUpdateView(UpdateView):
     model = Organization
     form_class = OrganizationForm
     template_name = 'org_form.html'
